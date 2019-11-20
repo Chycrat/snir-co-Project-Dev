816ccd6b87f612156f8c7118da51ad8a9f0e507e
allan <all.brochard@gmail.com>
2019-11-20 11:07:00 +0100

    <FIX><suppressions de la ligne git pull sur jenkins>

--- pull.sh
+++ pull.sh
@@ -1,6 +1,7 @@
 echo "La machine de dev effectue un Git Pull......."
-git pull -u origin master
+
 cd Back
 composer update
 php bin/console cache:clear --env=prod
 php bin/console cache:clear
+cd ..
