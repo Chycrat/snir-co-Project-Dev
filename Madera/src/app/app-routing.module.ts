e05bdf48f108b315645fd0e3af0fd48b1415dba6
Uprex <valentinhallay@gmail.com>
2019-11-20 10:23:06 +0100

    Création projet Angular front
    Angular 8, ajout angular material

---
+++ Madera/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { Routes, RouterModule } from '@angular/router';
+import { LoginComponent } from './login/login.component';
+import { AppComponent } from './app.component';
+
+
+const routes: Routes = [
+  { path: 'Login', component: LoginComponent },
+  { path: '**', component: AppComponent }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
