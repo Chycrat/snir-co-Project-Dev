6e91640dfcd64c44c130c6500fb05a039cb5febf
allan <all.brochard@gmail.com>
2020-02-10 10:04:46 +0100

    <FIX><Modification des controller et implementation des action dans client controller>

--- Madera/src/Controller/ProjetController.php
+++ Madera/src/Controller/ProjetController.php
@@ -5,15 +5,15 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 
-class ListePlanController extends AbstractController
+class ProjetController extends AbstractController
 {
     /**
-     * @Route("/listePlan", name="liste_plan")
+     * @Route("/listeProjet", name="liste_projet")
      */
     public function index()
     {
-        return $this->render('listePlan.html.twig', [
-            'controller_name' => 'ListePlanController',
+        return $this->render('listeProjet.html.twig', [
+            'controller_name' => 'ProjetController',
         ]);
     }
 }
