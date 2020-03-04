d390fd570087e8c6cbf4c91877381c610e3a9c50
Uprex <valentinhallay@gmail.com>
2020-03-04 19:11:37 +0100

    Modif front + suppression des enregistrer inutile

--- Madera/src/Form/MaderaProjetType.php
+++ Madera/src/Form/MaderaProjetType.php
@@ -13,12 +13,10 @@ class MaderaProjetType extends AbstractType
     public function buildForm(FormBuilderInterface $builder, array $options)
     {
         $builder
-            ->add('id_projet')
             ->add('nom_projet')
             ->add('date_creation_projet')
             ->add('date_modification_projet')
             ->add('maderaCommercial')
-            ->add('Enregistrer',SubmitType::class)
         ;
     }
 
